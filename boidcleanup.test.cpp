e1ff0f9c9f2aea2b33aa49ab6ecfc737fb82a560
NasiLorenzo <146074174+NasiLorenzo@users.noreply.github.com>
2024-05-23 13:57:35 +0200

    implementazione test funzionante

--- boidcleanup.test.cpp
+++ boidcleanup.test.cpp
@@ -1,16 +1,19 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "boidcleanup.hpp"
 #include "doctest.h"
-namespace boids {
-
+using namespace boids;
 TEST_CASE("Testing rules")
 {
+
   paramms::repulsione  = 0.7;
   paramms::steering    = 0.1;
   paramms::coesione    = 0.1;
   paramms::neigh2      = 1000000;
   paramms::neigh_align = 1000000;
-
+  paramms::alpha=M_PI;
+  paramms::attraction=0;
+  paramms::speedlimit   = 80;
+  paramms::speedminimum = 2; 
   boidstate boid1;
   boid1.pos = {700., 0.};
   boid1.vel = {300., 0.};
@@ -24,27 +27,29 @@ TEST_CASE("Testing rules")
   boid4.pos = {1000., 0.};
   boid4.vel = {400., 0.};
   boidstate boid5;
-  boid4.pos = {200., 0.};
-  boid4.vel = {300., 0.};
+  boid5.pos = {200., 0.};
+  boid5.vel = {300., 0.};
   boidstate boid6;
-  boid4.pos = {300., 0.};
-  boid4.vel = {-100., 0.};
+  boid6.pos = {300., 0.};
+  boid6.vel = {-100., 0.};
   boidstate boid7;
-  boid4.pos = {450., 0.};
-  boid4.vel = {200., 0.};
+  boid7.pos = {450., 0.};
+  boid7.vel = {200., 0.};
   boidstate boid8;
-  boid4.pos = {50., 0.};
-  boid4.vel = {400., 0.};
+  boid8.pos = {50., 110.};
+  boid8.vel = {400., 0.};
   boidstate boid9;
-  boid4.pos = {120., 0.};
-  boid4.vel = {50., 0.};
+  boid9.pos = {120., 990.};
+  boid9.vel = {50., 0.};
   boidstate boid10;
-  boid4.pos = {-910., 0.};
-  boid4.vel = {-300., 0.};
+  boid10.pos = {910., 0.};
+  boid10.vel = {-300., 0.};
 
-  stormo set{boid1, boid2, boid3, boid4, boid5,
-             boid6, boid7, boid8, boid9, boid10};
+  stormo set{boid1, boid2, boid3, boid4, boid5, boid6, boid7, boid8, boid9, boid10};
+  std::random_device r;
+  std::default_random_engine eng(r());
   ensemble flock{set};
+
   flock.update();
   REQUIRE(flock.size_() == 10);
 
@@ -60,4 +65,4 @@ TEST_CASE("Testing rules")
   CHECK(flock.set_()[9].vel[0] == doctest::Approx(2550.077778));
 }
 
-} // namespace boids
+// namespace boids
